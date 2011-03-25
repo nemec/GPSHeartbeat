93141e0f9684ec5e24fd0cd1c64488239bf03090
Dan N <djnemec@gmail.com>
2011-03-24 22:15:06 -0500

    Updated.

--- src/nemec/GPSHeartbeat/DefaultActivity.java
+++ src/nemec/GPSHeartbeat/DefaultActivity.java
@@ -1,20 +1,32 @@
 package nemec.GPSHeartbeat;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.RadioButton;
-import android.widget.SeekBar;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class DefaultActivity extends Activity {
+public class DefaultActivity extends Activity{
     
 	private Intent svc;
+	private ArrayList<String> valS;
+	private ArrayList<Integer> valI;
+	private final Integer DEFAULT_INTERVAL=60;
+	private SharedPreferences.Editor prefsEditor;
 	
 	private OnClickListener onListener = new OnClickListener(){
 		public void onClick(View v){
@@ -40,16 +52,49 @@ public class DefaultActivity extends Activity {
         svcOn.setOnClickListener(onListener);
         svcOff.setOnClickListener(offListener);
         
-        /*intervals = new TreeMap<Integer, Integer>();
-        intervals.put(10, 	 0);
-        intervals.put(30,	 15);
-        intervals.put(1*60,	 30);
-        intervals.put(2*60,	 50);
-        intervals.put(6*60,	 60);
-        intervals.put(12*60, 80);
-        intervals.put(24*60, 100);*/
+    	String prefs_file = this.getString(R.string.prefs_file);
+    	SharedPreferences prefs = getSharedPreferences(prefs_file, 0);
+    	prefsEditor = prefs.edit();
         
-                
+        // Intervals are calculated in minutes.
+        valS = new ArrayList<String>();
+        valI = new ArrayList<Integer>();
+        valS.add("10m");valI.add(10);
+		valS.add("30m");valI.add(30);
+		valS.add("1h");valI.add(60);
+		valS.add("2h");valI.add(120);
+		valS.add("6h");valI.add(360);
+		valS.add("12h");valI.add(720);
+		valS.add("24h");valI.add(1440);
+        Spinner spin=(Spinner)findViewById(R.id.SpinnerInterval);
+        
+		ArrayAdapter<String> aa=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valS);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
+        
+        spin.setSelection(valI.indexOf(getInterval()));
+        
+        spin.setOnItemSelectedListener( new Spinner.OnItemSelectedListener(){
+        	public void onItemSelected(AdapterView<?> parent, View v, int position, long id){
+        		setInterval(valI.get(position));
+        	}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+        });
+    }
+    
+    private void setInterval(int val){
+    	String interval_key = this.getString(R.string.interval_key);
+    	prefsEditor.putInt(interval_key, val);
+    	prefsEditor.commit();
+    }
+    
+    private Integer getInterval(){
+    	String prefs_file = this.getString(R.string.prefs_file);
+    	String interval_key = this.getString(R.string.interval_key);
+    	SharedPreferences settings = getSharedPreferences(prefs_file, 0);
+    	return settings.getInt(interval_key, DEFAULT_INTERVAL);
     }
     
 }
--- src/nemec/GPSHeartbeat/HeartbeatService.java
+++ src/nemec/GPSHeartbeat/HeartbeatService.java
@@ -1,11 +1,17 @@
 package nemec.GPSHeartbeat;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -13,87 +19,71 @@ import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
-public class HeartbeatService extends Service implements LocationListener{
+public class HeartbeatService extends Service implements LocationListener, OnSharedPreferenceChangeListener{
 	
-	private Timer timer;
-	private int INTERVAL; // Interval is in minutes
+	private int INTERVAL; // Interval is in seconds
 	private final IBinder mBinder = new LocalBinder();
+	private LocationManager locationManager;
+	private String server_ip;
+	private int server_port;
 	
 	public HeartbeatService(){
-		this(60);
-	}
-	
-	public HeartbeatService(int interval){
-		timer = new Timer();
-		INTERVAL=interval;
-	}
-	
-	public void setInterval(int i){
-		INTERVAL = i;
-	}
-	
-	private Location getLocation() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        String provider = locationManager.getBestProvider(criteria, true);
 
-        // In order to make sure the device is getting location, request
-        // updates. locationManager.requestLocationUpdates(provider, 1, 0,
-        // this);
-        //locationManager.requestLocationUpdates(provider, 1, 0, this);
-        return locationManager.getLastKnownLocation(provider);
-    }
-
-	
-	private void postLocation(){
-		Location l = getLocation();
-		System.out.println(l);
 	}
 
 	public void onCreate() {
 		super.onCreate();
-		//startservice();
-		Toast.makeText(getApplicationContext(), 
-				"Starting service.", Toast.LENGTH_SHORT).show();
-
-
+		startservice();
 	}
 	
 	public void onDestroy(){
 		super.onDestroy();
-		//stopservice();
-
-		Toast.makeText(getApplicationContext(), 
-				"Stopping service.", Toast.LENGTH_SHORT).show();
+		stopservice();
 	}
 	
 	private void startservice() {
 		registerBootHook();
 		
-		timer.scheduleAtFixedRate( new TimerTask() {
-			public void run() {	
-				//postLocation();
-			}
-		}, 0, INTERVAL*60*1000);
+    	SharedPreferences prefs = getSharedPreferences(this.getString(R.string.prefs_file), 0);
+    	INTERVAL = prefs.getInt(this.getString(R.string.interval_key),
+    					60);
+    	server_ip = prefs.getString(this.getString(R.string.server_ip_key),
+    					"192.168.1.128");
+    	server_port = prefs.getInt(this.getString(R.string.server_port_key),
+    					9000);
+    	
+    	prefs.registerOnSharedPreferenceChangeListener(this);
+    	
+    	locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        locationManager.requestLocationUpdates(provider, 10000, 0, this);
+        
+        Toast.makeText(getApplicationContext(), 
+				"Starting service.", Toast.LENGTH_SHORT).show();
 	}
 	
-	private void registerBootHook(){
-		//TODO Start service at boot
-	}
+		private void registerBootHook(){
+			//TODO Start service at boot
+		}
 	
 	private void stopservice() {
 		unregisterBootHook();
-		if (timer != null){
-			timer.cancel();
-		}
+		
+		locationManager.removeUpdates(this);
+		
+		Toast.makeText(getApplicationContext(), 
+				"Stopping service.", Toast.LENGTH_SHORT).show();
 	}
 	
-	private void unregisterBootHook(){
-		//TODO Stop service from starting at boot. 
-	}
+		private void unregisterBootHook(){
+			//TODO Stop service from starting at boot. 
+		}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -106,11 +96,34 @@ public class HeartbeatService extends Service implements LocationListener{
 		}
 		
 	}
+	
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key){
+		if(key == HeartbeatService.this.getString(R.string.interval_key)){
+			int newInterval = prefs.getInt(key, -1);
+			if(newInterval > 0){
+				INTERVAL = newInterval;
+			}
+			Log.v("PrefsChanged", "Interval has been changed: "+ newInterval);
+		}
+				
+	}
 
 	@Override
-	public void onLocationChanged(Location arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onLocationChanged(Location l) {
+		String data = "(" + l.getLatitude() + ", " + l.getLongitude() + ")";
+		Log.v("Location", data);
+		try {
+			Socket connection = new Socket(server_ip, 9000);
+			PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+			out.println(data);
+			connection.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
